5ac365cb3444cd0dacffdeb2119bb889d56c49d6
Znurre <lova@avoidpointer.com>
2023-09-16 11:49:52 +0200

    Foam stuff

--- examples/foam/sdl/sdl.c
+++ examples/foam/sdl/sdl.c
@@ -1,9 +1,12 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 #include <stdio.h>
 
 struct Event
 {
+    uint32_t type;
+
     SDL_KeyboardEvent *key;
     SDL_MouseMotionEvent *motion;
     SDL_MouseButtonEvent *button;
@@ -14,6 +17,8 @@ struct Event *SDL_WaitEventEx(struct Event *events)
     SDL_Event event;
 
     SDL_WaitEvent(&event);
+
+    events->type = event.type;
 
     memcpy(events->key, &event.key, sizeof(SDL_KeyboardEvent));
     memcpy(events->motion, &event.motion, sizeof(SDL_MouseMotionEvent));
@@ -65,4 +70,26 @@ void printBuf(char *buffer)
 void *boo()
 {
     return 0;
+}
+
+SDL_Surface *RenderText_Solid(TTF_Font *font, const char *text, int r, int g, int b)
+{
+    if (!font)
+    {
+        printf("no font!!\n");
+    }
+
+    SDL_Color color = { r, g, b, 0 };
+
+    return TTF_RenderText_Blended(font, text, color);
+}
+
+int GetSurfaceWidth(SDL_Surface *surface)
+{
+    return surface->w;
+}
+
+int GetSurfaceHeight(SDL_Surface *surface)
+{
+    return surface->h;
 }
